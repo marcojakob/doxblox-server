a5c7e172b8f25abd2bca6046831231387b25e4ea
marcojakob <majakob@gmx.ch>
2013-10-29 15:58:12 +0100

    Remove tests

---
+++ doxblox-client/test/ui/navigation/tree_tests.dart
@@ -0,0 +1,21 @@
+part of ui_tests;
+
+treeTests() {
+  
+  group('Tree Tests:', () {
+  
+//  test('equals_SameNode_ReturnsTrue', () {
+//    // given
+//    TreeNode node1 = new TreeNode('1', 'aaa', 'document');
+//    TreeNode node2 = new TreeNode('1', 'aaa', 'document');
+//    
+//    // when
+//    bool same = node1 == node2;
+//    
+//    // then
+//    expect(same, isTrue);
+//  });
+  
+  });
+}
+
--- doxblox-client/test/ui/ui_tests.dart
+++ doxblox-client/test/ui/ui_tests.dart
@@ -2,10 +2,8 @@ library ui_tests;
 
 import 'package:unittest/unittest.dart';
 
-import '../../web/ui/navigation/tree_view.dart';
-
-part 'navigation/tree_node_tests.dart';
+part 'navigation/tree_tests.dart';
 
 main() {
-  treeNodeTests();
+  treeTests();
 }
